ad15a60d3289401552d55fb02b5a95902a7afa4f
tanapruk <trust@nextzy.com>
2018-08-07 18:53:31 +0700

    first commit

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const config = {
+  apiKey: 'AIzaSyBrb7203i1OXieBBddOxWLaFfid7vT0ORc',
+  authDomain: 'reactfirebaseauthtutoria-13975.firebaseapp.com',
+  databaseURL: 'https://reactfirebaseauthtutoria-13975.firebaseio.com',
+  projectId: 'reactfirebaseauthtutoria-13975',
+  storageBucket: 'reactfirebaseauthtutoria-13975.appspot.com',
+  messagingSenderId: '134237114276'
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+const auth = firebase.auth()
+export { auth }
